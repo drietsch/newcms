24272c86df5ed0b255b8edce2c14e6166ac7413d
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-08-14 08:51:26 +0000

    move to sqldumps folder

--- sql_dumps/Version_60xx/tblFailedLogins.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tblFailedLogins (
-  ID bigint(20) NOT NULL default '0',
-  Username varchar(64) NOT NULL default '',
-  `Password` varchar(32) NOT NULL default '',
-  IP varchar(15) NOT NULL default '',
-  LoginDate int(11) NOT NULL default '0'
-) TYPE=MyISAM;
--- sql_dumps/Version_60xx/tblbannerclicks.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tblbannerclicks (
-  ID bigint(20) NOT NULL default '0',
-  `Timestamp` bigint(20) default NULL,
-  IP varchar(30) NOT NULL default '',
-  Referer varchar(255) NOT NULL default '',
-  DID bigint(20) NOT NULL default '0',
-  Page varchar(255) NOT NULL default ''
-) TYPE=MyISAM;
