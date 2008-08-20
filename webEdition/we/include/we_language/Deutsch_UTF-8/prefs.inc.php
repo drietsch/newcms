1ac4a3aee93193d73be4bd75b7671e0dc78b67c0
damjan.denic <damjan.denic@5781a423-4729-0410-aacd-f88070b95415>
2008-08-20 09:08:19 +0000

    #temprery remove econa text

--- webEdition/we/include/we_language/Deutsch_UTF-8/prefs.inc.php
+++ webEdition/we/include/we_language/Deutsch_UTF-8/prefs.inc.php
@@ -342,12 +342,6 @@ $l_prefs["tab_versions"] = 'Versionierung';
 	 */
 	$l_prefs["pagelogger_dir"] = "pageLogger-Verzeichnis";
 	
-	/**
-	 * ECONDA
-	 */
-	$l_prefs["econda_information"] = "econda ist der Spezialist für erfolgreiches Web Shop Controlling. <br><a href='http://www.econda.de/ssa/shop.php' onclick='window.open(this.href,\"econda\",\"width=800,height=600,resizable=yes,menubar=yes,scrollbars=yes,status=yes,toolbar=yes\"); return false'>Online-Antrag</a>";
-	$l_prefs["econda_enable"] = "Econda aktivieren";
-
 /*****************************************************************************
  * ERROR HANDLING
  *****************************************************************************/
