935c001e367b2044ae03d2f08dfc63f3616aca02
alexander.lindenstruth <alexander.lindenstruth@5781a423-4729-0410-aacd-f88070b95415>
2008-08-21 14:59:01 +0000

    language files updated

---
+++ webEdition/apps/toolfactory/language/language_Dutch_UTF-8.inc.php
@@ -0,0 +1,6 @@
+<?php
+			
+include($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/Dutch_UTF-8/tools.inc.php');
+include($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/Dutch_UTF-8/toolfactory.inc.php');
+
+?>
