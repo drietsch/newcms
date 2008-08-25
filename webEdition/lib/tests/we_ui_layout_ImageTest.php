cf5ebac8782fc575046553510cfc0601a495bd80
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-08-25 13:38:15 +0000

    add unit test

--- webEdition/lib/tests/we_ui_layout_ImageTest.php
+++ webEdition/lib/tests/we_ui_layout_ImageTest.php
@@ -17,6 +17,14 @@ class we_ui_layout_ImageTest extends PHPUnit_Framework_TestCase
 	{
 		$this->assertEquals(we_ui_layout_Image::getPixel(5,4), '<img src="/webEdition/lib/we/ui/layout/img/pixel.gif" width="5" height="4" alt=""/>');
 	}
+	
+	/**
+	 * Tests we_ui_layout_Image::getIconClass()
+	 */
+	public function testGetIconClass()
+	{
+		$this->assertEquals(we_ui_layout_Image::getIconClass('application/*','.pdf'), 'pdf');
+	}
 
 }
 
