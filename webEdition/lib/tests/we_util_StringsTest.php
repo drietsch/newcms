83a40b884f1bced1fc1417672773c5b6bd1b70ee
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-08-25 13:38:40 +0000

    add unit test

--- webEdition/lib/tests/we_util_StringsTest.php
+++ webEdition/lib/tests/we_util_StringsTest.php
@@ -18,5 +18,38 @@ class we_util_StringsTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(strlen(we_util_Strings::createUniqueId(12)), 12);
 	}
 
+	
+	/**
+	 * Tests we_util_Strings::makeCSVFromArray()
+	 */
+	public function testMakeCSVFromArray()
+	{
+		$this->assertEquals(we_util_Strings::makeCSVFromArray(array("x","y")), "x,y");
+	}
+	
+	/**
+	 * Tests we_util_Strings::makeArrayFromCSV()
+	 */
+	public function testMakeArrayFromCSV()
+	{
+		$this->assertEquals(we_util_Strings::makeArrayFromCSV("x,y"), array("x","y"));
+	}
+	
+	/**
+	 * Tests we_util_Strings::quoteForJSString()
+	 */
+	public function testQuoteForJSString()
+	{
+		$this->assertEquals(we_util_Strings::quoteForJSString("test'test"), "test\'test");
+	}
+	
+	/**
+	 * Tests we_util_Strings::shortenPath()
+	 */
+	public function testShortenPath()
+	{
+		$this->assertEquals(we_util_Strings::shortenPath("Averylonghlongword", 12), "Aver....word");
+	}
+
 }
 
