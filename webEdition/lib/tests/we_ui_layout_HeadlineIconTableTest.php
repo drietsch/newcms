68908f05005b5f46283d93f79d567056c12772da
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-08-25 13:37:47 +0000

    add unit test

--- webEdition/lib/tests/we_ui_layout_HeadlineIconTableTest.php
+++ webEdition/lib/tests/we_ui_layout_HeadlineIconTableTest.php
@@ -21,12 +21,10 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 	protected function setUp()
 	{
 		parent::setUp();
-
-		$inp = new we_ui_controls_TextField(array('id' => 'test', 'name'=>'test', 'value'=>'default', 'width'=>100));
-		$inp1 = new we_ui_controls_TextField(array('id' => 'test1', 'name'=>'test1', 'value'=>'default', 'width'=>100));
-		$inp2 = new we_ui_controls_TextField(array('id' => 'test2', 'name'=>'test2', 'value'=>'default', 'width'=>100));
 		
-		
+		$inp = new we_ui_controls_TextField(array('id' => 'test', 'name' => 'test', 'value' => 'default', 'width' => 100));
+		$inp1 = new we_ui_controls_TextField(array('id' => 'test1', 'name' => 'test1', 'value' => 'default', 'width' => 100));
+		$inp2 = new we_ui_controls_TextField(array('id' => 'test2', 'name' => 'test2', 'value' => 'default', 'width' => 100));
 		
 		$rows = array();
 		$row = new we_ui_layout_HeadlineIconTableRow(array('id' => 'rowid1', 'title' => 'Input 1', 'iconPath' => we_ui_layout_HeadlineIconTableRow::kIconAttributes));
@@ -38,10 +36,8 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 		$row->addElement($inp2);
 		$rows[] = $row;
 		
-		
-		$this->we_ui_layout_HeadlineIconTable = new we_ui_layout_HeadlineIconTable(array('id' => 'tableId', 'rows'=>$rows, 'width'=>'100%', 'title'=>'TEST Titel'));
+		$this->we_ui_layout_HeadlineIconTable = new we_ui_layout_HeadlineIconTable(array('id' => 'tableId', 'foldedText' => 'hello', 'unfoldedText' => 'mytext', 'unfoldWhenRenders' => 'unfold', 'rows' => $rows, 'width' => '100%', 'title' => 'TEST Titel'));
 		$this->we_ui_layout_HeadlineIconTable->setMarginLeft(40);
-		
 	
 	}
 
@@ -50,10 +46,7 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testGetButtonTable()
 	{
-		// TODO Auto-generated we_ui_layout_HeadlineIconTableTest->testGetButtonTable()
-		$this->markTestIncomplete("getButtonTable test not implemented");
-		
-		$this->we_ui_layout_HeadlineIconTable->getButtonTable(/* parameters */);
+		$this->assertEquals($this->we_ui_layout_HeadlineIconTable->getButtonTable(), '');
 	
 	}
 
@@ -62,10 +55,7 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testGetFoldAtIndex()
 	{
-		// TODO Auto-generated we_ui_layout_HeadlineIconTableTest->testGetFoldAtIndex()
-		$this->markTestIncomplete("getFoldAtIndex test not implemented");
-		
-		$this->we_ui_layout_HeadlineIconTable->getFoldAtIndex(/* parameters */);
+		$this->assertEquals($this->we_ui_layout_HeadlineIconTable->getFoldAtIndex(), -1);
 	
 	}
 
@@ -74,10 +64,7 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testGetFoldedText()
 	{
-		// TODO Auto-generated we_ui_layout_HeadlineIconTableTest->testGetFoldedText()
-		$this->markTestIncomplete("getFoldedText test not implemented");
-		
-		$this->we_ui_layout_HeadlineIconTable->getFoldedText(/* parameters */);
+		$this->assertEquals($this->we_ui_layout_HeadlineIconTable->getFoldedText(), 'hello');
 	
 	}
 
@@ -102,10 +89,7 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testGetUnfoldedText()
 	{
-		// TODO Auto-generated we_ui_layout_HeadlineIconTableTest->testGetUnfoldedText()
-		$this->markTestIncomplete("getUnfoldedText test not implemented");
-		
-		$this->we_ui_layout_HeadlineIconTable->getUnfoldedText(/* parameters */);
+		$this->assertEquals($this->we_ui_layout_HeadlineIconTable->getUnfoldedText(), 'mytext');
 	
 	}
 
@@ -114,13 +98,9 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testGetUnfoldWhenRenders()
 	{
-		// TODO Auto-generated we_ui_layout_HeadlineIconTableTest->testGetUnfoldWhenRenders()
-		$this->markTestIncomplete("getUnfoldWhenRenders test not implemented");
-		
-		$this->we_ui_layout_HeadlineIconTable->getUnfoldWhenRenders(/* parameters */);
+		$this->assertEquals($this->we_ui_layout_HeadlineIconTable->getUnfoldWhenRenders(), 'unfold');
 	
 	}
-
 
 	/**
 	 * Tests we_ui_layout_HeadlineIconTableRow->hasLine()
@@ -135,7 +115,7 @@ class we_ui_layout_HeadlineIconTableTest extends PHPUnit_Framework_TestCase
 		<td><img src="/webEdition/lib/we/ui/layout/img/pixel.gif" width="2" height="8" alt=""/></td>
 	</tr>
 	<tr>
-		<td id="tableId_td"><div style="margin-left:40px" id="tableId_div_0"><div style="float:left;width:150px"><img src="/webEdition/images/icons/attrib.gif" alt="" /></div><div style="float:left;"><div class="we_ui_layout_HeadlineIconTable_RowTitle" style="margin-bottom:10px;">Input 1</div><div><input id="test" name="test" value="default" type="text" style="width:100px;" class="we_ui_controls_TextInput" onFocus="this.className=&quot;we_ui_controls_TextInput_Selected&quot;;" onBlur="this.className=&quot;we_ui_controls_TextInput&quot;;"/><input id="test1" name="test1" value="default" type="text" style="width:100px;" class="we_ui_controls_TextInput" onFocus="this.className=&quot;we_ui_controls_TextInput_Selected&quot;;" onBlur="this.className=&quot;we_ui_controls_TextInput&quot;;"/></div></div><br style="clear:both;"></div><div class="we_ui_layout_HeadlineIconTable_Rule"></div><div style="margin-left:40px" id="tableId_div_1"><div style="float:left;width:150px"><img src="/webEdition/images/icons/cache.gif" alt="" /></div><div style="float:left;"><div class="we_ui_layout_HeadlineIconTable_RowTitle" style="margin-bottom:10px;">Input 2</div><div><input id="test2" name="test2" value="default" type="text" style="width:100px;" class="we_ui_controls_TextInput" onFocus="this.className=&quot;we_ui_controls_TextInput_Selected&quot;;" onBlur="this.className=&quot;we_ui_controls_TextInput&quot;;"/></div></div><br style="clear:both;"></div><div class="we_ui_layout_HeadlineIconTable_Space"></div></td>
+		<td id="tableId_td"><div style="margin-left:40px" id="tableId_div_0"><div style="float:left;width:150px"><img src="/webEdition/images/icons/attrib.gif" alt="" /></div><div style="float:left;"><div class="we_ui_layout_HeadlineIconTable_RowTitle" style="margin-bottom:10px;">Input 1</div><div><input id="test" name="test" value="default" type="text" style="width:96px;height:17px;" class="we_ui_controls_TextInput" onFocus="YAHOO.util.Dom.addClass(this, &quot;we_ui_controls_TextInput_Selected&quot;);" onBlur="YAHOO.util.Dom.removeClass(this, &quot;we_ui_controls_TextInput_Selected&quot;);"/><input id="test1" name="test1" value="default" type="text" style="width:96px;height:17px;" class="we_ui_controls_TextInput" onFocus="YAHOO.util.Dom.addClass(this, &quot;we_ui_controls_TextInput_Selected&quot;);" onBlur="YAHOO.util.Dom.removeClass(this, &quot;we_ui_controls_TextInput_Selected&quot;);"/></div></div><br style="clear:both;"></div><div class="we_ui_layout_HeadlineIconTable_Rule"></div><div style="margin-left:40px" id="tableId_div_1"><div style="float:left;width:150px"><img src="/webEdition/images/icons/cache.gif" alt="" /></div><div style="float:left;"><div class="we_ui_layout_HeadlineIconTable_RowTitle" style="margin-bottom:10px;">Input 2</div><div><input id="test2" name="test2" value="default" type="text" style="width:96px;height:17px;" class="we_ui_controls_TextInput" onFocus="YAHOO.util.Dom.addClass(this, &quot;we_ui_controls_TextInput_Selected&quot;);" onBlur="YAHOO.util.Dom.removeClass(this, &quot;we_ui_controls_TextInput_Selected&quot;);"/></div></div><br style="clear:both;"></div><div class="we_ui_layout_HeadlineIconTable_Space"></div></td>
 	</tr>
 	<tr>
 		<td></td>
