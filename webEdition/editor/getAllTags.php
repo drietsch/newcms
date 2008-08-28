b29646577c8e8a145a41e05156e5a129bd7acbcd
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-08-28 09:16:15 +0000

    sdfsdf

--- webEdition/editor/getAllTags.php
+++ webEdition/editor/getAllTags.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   we
+ * @package    we_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
 $_dir = $_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/we_tags';
 
