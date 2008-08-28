3637f6b7a8363bb715cab7c001135d9e7d3cc87c
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-08-28 09:30:44 +0000

    Copyright and License text included

--- webEdition/js/layers/layersNS.js
+++ webEdition/js/layers/layersNS.js
@@ -1,12 +1,14 @@
-// +----------------------------------------------------------------------+
-// | webEdition                                                           |
-// +----------------------------------------------------------------------+
-// | PHP version 4.1.0 or greater                                         |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2000 - 2007 living-e AG                                |
-// +----------------------------------------------------------------------+
-//
-// $Id: layersNS.js,v 1.6 2007/05/23 15:39:32 holger.meyer Exp $
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   we
+ * @package    we_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
 function showLayer(layer) {
 	if (layer)
