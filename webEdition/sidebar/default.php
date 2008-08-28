a37cabd2a28a2129247912f91c55023251b1082a
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-08-28 10:31:18 +0000

    Copyright and License text included

--- webEdition/sidebar/default.php
+++ webEdition/sidebar/default.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   we
+ * @package    we_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
-	protect();
+protect();
 
 ?><html>
 
