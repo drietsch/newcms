2bf925dac3c807a6b1ddcf52426ea75ff826a3a9
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-08-28 09:31:03 +0000

    Copyright and License text included

--- webEdition/js/external/weCheckForm.js
+++ webEdition/js/external/weCheckForm.js
@@ -1,4 +1,17 @@
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   we
+ * @package    we_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
+
 function weCheckFormEvent(){}
+
 weCheckFormEvent.addEvent = function(e, name, f) {
     if (e.addEventListener) {
         e.addEventListener(
