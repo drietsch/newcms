17eb9f8b1ef458ca66de35aa72971f7fa4b77219
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 09:53:58 +0000

    add code documentation

--- webEdition/apps/toolfactory/controllers/RpcController.php
+++ webEdition/apps/toolfactory/controllers/RpcController.php
@@ -1,8 +1,17 @@
 <?php
-
+/**
+ * Base Rpc Controller
+ * 
+ * @category   app
+ * @package    app_controller
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/license     LICENSE_TYPE  TODO insert license type and url
+ */
 class RpcController extends Zend_Controller_Action
 {
-
+	/**
+	 * The default action
+	 */
 	public function indexAction()
 	{
 		$jsonOutput = we_net_rpc_JsonRpc::getReply('toolfactory');
