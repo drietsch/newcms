2d1f169dcbdf9ed237cbef9ec756150d957f3f91
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 09:54:42 +0000

    add code documentation

--- webEdition/apps/toolfactory/controllers/FramesetController.php
+++ webEdition/apps/toolfactory/controllers/FramesetController.php
@@ -1,14 +1,18 @@
 <?php
 
-/**
- * FramesetController
- * 
- * @author
- * @version 
+/*
+ * @see we_app_controller_FramesetAction
  */
-
 Zend_Loader::loadClass('we_app_controller_FramesetAction');
 
+/**
+ * Base Frameset Controller
+ * 
+ * @category   app
+ * @package    app_controller
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/license     LICENSE_TYPE  TODO insert license type and url
+ */
 class FramesetController extends we_app_controller_FramesetAction
 {
 
