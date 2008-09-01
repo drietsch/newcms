2962ac4a2862b35d9b070dfee59ddf2e78933d99
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 09:55:12 +0000

    add code documentation

--- webEdition/apps/toolfactory/controllers/CmdController.php
+++ webEdition/apps/toolfactory/controllers/CmdController.php
@@ -1,14 +1,18 @@
 <?php
 
-/**
- * CmdController
- * 
- * @author
- * @version 
+/*
+ * @see we_app_controller_CmdAction
  */
-
 Zend_Loader::loadClass('we_app_controller_CmdAction');
 
+/**
+ * Base Action Controller
+ * 
+ * @category   app
+ * @package    app_controllers
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/license     LICENSE_TYPE  TODO insert license type and url
+ */
 class CmdController extends we_app_controller_CmdAction
 {
 
