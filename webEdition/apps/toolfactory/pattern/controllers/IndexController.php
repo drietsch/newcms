64fc741dc1a6df6c7c6042498cb242ac94ea27a5
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 09:52:06 +0000

    add code documentation

--- webEdition/apps/toolfactory/pattern/controllers/IndexController.php
+++ webEdition/apps/toolfactory/pattern/controllers/IndexController.php
@@ -1,6 +1,16 @@
-
+/*
+ * @see we_app_controller_IndexAction
+ */
 Zend_Loader::loadClass('we_app_controller_IndexAction');
 
+/**
+ * Base Index Controller
+ * 
+ * @category   we
+ * @package    app_controller
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/license     LICENSE_TYPE  TODO insert license type and url
+ */
 class IndexController extends we_app_controller_IndexAction
 {
 
