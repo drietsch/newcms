d30ec91012b0a58ba9b4008aa1b3c9554f860479
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 10:06:44 +0000

    remove tools.inc.php include

--- webEdition/we/include/we_tools/navigation/language/language_Norwegian_UTF-8.inc.php
+++ webEdition/we/include/we_tools/navigation/language/language_Norwegian_UTF-8.inc.php
@@ -9,7 +9,7 @@
 // +----------------------------------------------------------------------+
 //
 
-include($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/' . $GLOBALS['WE_LANGUAGE'] . '/tools.inc.php');
+
 include($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/' . $GLOBALS['WE_LANGUAGE'] . '/navigation.inc.php');
 
 ?>
