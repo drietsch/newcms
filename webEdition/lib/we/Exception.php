1d49b3b5f570c6e0eeb75ccf93fd8b7082a10412
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 10:29:16 +0000

    add code documentation

--- webEdition/lib/we/Exception.php
+++ webEdition/lib/we/Exception.php
@@ -1,14 +1,35 @@
 <?php
-
-include_once (dirname(dirname(__FILE__)) . '/we/core/autoload.php');
-require_once 'Zend/Exception.php';
 /**
- * @see we_util_Log
- */
-Zend_Loader::loadClass('we_util_Log');
+ * webEdition SDK
+ *
+ * LICENSE_TEXT
+ *
+ * TODO insert license text
+ * @category   we
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENCE_TYPE  TODO insert license type and url
+ */ 
 
+
+/**
+ * @see Zend_Exception
+ */
+Zend_Loader::loadClass('Zend_Exception');
+
+/**
+ * class for exceptions
+ * 
+ * @category   we
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/license     LICENSE_TYPE  TODO insert license type and url
+ */
 class we_Exception extends Zend_Exception
 {
+	/**
+	 * write exception into errorlog
+	 * 
+	 * @return void
+	 */
 	public function __construct() {
 		we_util_Log::errorLog("webEdition exception '".get_class($this)."' in ".$this->getFile().":".$this->getLine()."\nStack trace;\n".$this->getTraceAsString());
 	}
