9d3c081e8c1c213c88cd4f5c67a30c919aa77cc1
thomas.kneip <thomas.kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-09-01 09:55:28 +0000

    add code documentation

--- webEdition/apps/toolfactory/conf/meta.conf.php
+++ webEdition/apps/toolfactory/conf/meta.conf.php
@@ -1,15 +1,19 @@
 <?php
-						
-	include_once('define.conf.php');
-	
-	$metaInfo = array(
-		'name' => 'toolfactory',
-		'realname' => 'toolfactory',
-		'classname'=>'toolfactory',
-		'maintable'=>'',
-		'datasource'=>'custom:',
-		'startpermission'=>'EDIT_APP_TOOLFACTORY'
-	);
+// include autoload function
+include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/lib/we/core/autoload.php');
 
+Zend_Loader::loadClass('we_core_Local');
 
+$translate = we_core_Local::addTranslation('apps.xml');
+we_core_Local::addTranslation('default.xml', 'toolfactory');
 
+include_once('define.conf.php');
+
+$metaInfo = array(
+	'name' => 'toolfactory',
+	'realname' => $translate->_('toolfactory'),
+	'classname'=>'toolfactory',
+	'maintable'=>'',
+	'datasource'=>'custom:',
+	'startpermission'=>'EDIT_APP_TOOLFACTORY'
+);
