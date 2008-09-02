07f699861097372cc83b476d5b81a60dd2288c5c
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 15:42:46 +0000

    sfd

--- webEdition/wysiwyg/weWysiwygSafari.js
+++ webEdition/wysiwyg/weWysiwygSafari.js
@@ -4,8 +4,8 @@
  * LICENSETEXT_CMS
  *
  *
- * @category   we
- * @package    we_wysiwyg
+ * @category   webEdition
+ * @package    webEdition_wysiwyg
  * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
  * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
  */
