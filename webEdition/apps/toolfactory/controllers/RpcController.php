8432d697101e134355e3e6c791b4b4b1ccafe96f
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 16:12:32 +0000

    asd

--- webEdition/apps/toolfactory/controllers/RpcController.php
+++ webEdition/apps/toolfactory/controllers/RpcController.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Base Rpc Controller
- * 
- * @category   app
- * @package    app_controller
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_toolfactory
  * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
- * @license    http://www.living-e.de/license     LICENSE_TYPE  TODO insert license type and url
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
  */
+
 class RpcController extends Zend_Controller_Action
 {
 	/**
