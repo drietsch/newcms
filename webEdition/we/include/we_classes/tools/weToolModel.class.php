854ad638e51c8650ac91a9251b6e8c281a80b37c
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 15:58:13 +0000

    asd

--- webEdition/we/include/we_classes/tools/weToolModel.class.php
+++ webEdition/we/include/we_classes/tools/weToolModel.class.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
 include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we_classes/modules/weModelBase.php');
 
