66836096438d5d06c83b3fd49abb6434acbb8fca
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 15:45:48 +0000

    dfg

--- webEdition/we/include/we_message_reporting/we_message_reporting.class.php
+++ webEdition/we/include/we_message_reporting/we_message_reporting.class.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
 // contansts for messaging!
 // these are binray checked like permissions in unix, DON'T change indexes
