f844b16806e16fde97ca878568caada8de15f527
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 16:20:05 +0000

    dfg

--- webEdition/apps/toolfactory/service/views/json/rpctoolfactoryView.class.php
+++ webEdition/apps/toolfactory/service/views/json/rpctoolfactoryView.class.php
@@ -1,5 +1,16 @@
 <?php
-			
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_toolfactory
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
+
 class rpctoolfactoryView extends rpcView {
 	
 	
