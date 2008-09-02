cd23d33d334841775bfde1e39f5f922f73d12cbf
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 15:31:49 +0000

    sdf

--- webEdition/we/include/weInfoPages/weNoResource.inc.php
+++ webEdition/we/include/weInfoPages/weNoResource.inc.php
@@ -1,18 +1,19 @@
-<?php                                                                             
+<?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
-// +----------------------------------------------------------------------+
-// | webEdition                                                           |
-// +----------------------------------------------------------------------+
-// | PHP version 4.1.0 or greater                                         |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2000 - 2007 living-e AG                                |
-// +----------------------------------------------------------------------+
-//
-
-
-include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/"."we.inc.php");
-include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/"."we_html_tools.inc.php");
-include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we_language/".$GLOBALS["WE_LANGUAGE"]."/alert.inc.php");
+include_once ($_SERVER["DOCUMENT_ROOT"] . "/webEdition/we/include/" . "we.inc.php");
+include_once ($_SERVER["DOCUMENT_ROOT"] . "/webEdition/we/include/" . "we_html_tools.inc.php");
+include_once ($_SERVER["DOCUMENT_ROOT"] . "/webEdition/we/include/we_language/" . $GLOBALS["WE_LANGUAGE"] . "/alert.inc.php");
 
 protect();
 
@@ -20,7 +21,7 @@ htmlTop();
 
 print STYLESHEET;
 
-$content = "<p class=\"defaultfont\">".$l_alert["noResource"]."</p>";
+$content = "<p class=\"defaultfont\">" . $l_alert["noResource"] . "</p>";
 
 ?>
 <script language="JavaScript" type="text/javascript">
@@ -28,11 +29,11 @@ top.toggleBusy(0);
 var _EditorFrame = top.weEditorFrameController.getEditorFrame(window.name);
 _EditorFrame.setEditorIsLoading(false);
 </script>
-	</head>
+</head>
 
-    <body class="weDialogBody">
+<body class="weDialogBody">
 <?php
-print htmlDialogLayout($content,$l_alert["noResourceTitle"] );
+print htmlDialogLayout($content, $l_alert["noResourceTitle"]);
 ?>
     </body>
 </html>
