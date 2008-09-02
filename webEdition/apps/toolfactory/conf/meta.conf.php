96186f09f406e93aaa11ae9a8b393cea125b8505
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 16:10:19 +0000

    sdf

--- webEdition/apps/toolfactory/conf/meta.conf.php
+++ webEdition/apps/toolfactory/conf/meta.conf.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_toolfactory
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
+
 // include autoload function
 include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/lib/we/core/autoload.php');
 
