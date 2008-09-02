92fed87e5ad275e94bb97c422da68553da0a46bc
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-02 16:26:42 +0000

    sdf

--- webEdition/apps/toolfactory/views/scripts/frameset/right.php
+++ webEdition/apps/toolfactory/views/scripts/frameset/right.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_toolfactory
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
+
 $appDir = Zend_Controller_Front::getInstance()->getParam('appDir');
 
 $client = we_ui_Client::getInstance();
