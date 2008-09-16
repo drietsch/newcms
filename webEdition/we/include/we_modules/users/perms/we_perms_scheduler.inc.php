1ff38ce6c608cd081b2e1964c806db4d46f06ccd
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-16 09:06:22 +0000

    sdf

--- webEdition/we/include/we_modules/users/perms/we_perms_scheduler.inc.php
+++ webEdition/we/include/we_modules/users/perms/we_perms_scheduler.inc.php
@@ -1,13 +1,16 @@
 <?php
              
-// +----------------------------------------------------------------------+
-// | webEdition                                                           |
-// +----------------------------------------------------------------------+
-// | PHP version 4.1.0 or greater                                         |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2000 - 2007 living-e AG                                |
-// +----------------------------------------------------------------------+
-//
+/**
+ * webEdition CMS
+ *
+ * LICENSETEXT_CMS
+ *
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.living-e.de/licence     LICENSETEXT_CMS  TODO insert license type and url
+ */
 
 
 include($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we_language/" . $GLOBALS["WE_LANGUAGE"] . "/modules/perms/scheduler.inc.php");
