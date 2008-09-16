0cca85de094efcc32986eb8fc65b17f435f2b485
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-16 08:40:53 +0000

    dfg

--- webEdition/multiEditor/EditorFrameController.php
+++ webEdition/multiEditor/EditorFrameController.php
@@ -14,14 +14,6 @@
 include_once($_SERVER["DOCUMENT_ROOT"].'/webEdition/we/include/we.inc.php');
 include_once($_SERVER["DOCUMENT_ROOT"].'/webEdition/we/include/we_language/' . $GLOBALS["WE_LANGUAGE"] .  '/multiEditor.inc.php');
 
-// +----------------------------------------------------------------------+
-// | webEdition                                                           |
-// +----------------------------------------------------------------------+
-// | PHP version 4.1.0 or greater                                         |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2000 - 2007 living-e AG                                |
-// +----------------------------------------------------------------------+
-
 ?><html>
 <head>
 <script type="text/javascript">
