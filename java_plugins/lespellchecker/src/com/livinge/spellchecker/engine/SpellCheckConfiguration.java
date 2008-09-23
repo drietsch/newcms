256ec1ac387c8ea5e1bc4eb97a87180bc06abda6
holger.meyer <holger.meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-09-23 16:09:43 +0000

    sdf

--- java_plugins/lespellchecker/src/com/livinge/spellchecker/engine/SpellCheckConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.livinge.spellchecker.engine;
-
-import com.swabunga.spell.engine.PropertyConfiguration;
-
-public class SpellCheckConfiguration extends PropertyConfiguration {
-
-	// read custom properties
-	
-}
