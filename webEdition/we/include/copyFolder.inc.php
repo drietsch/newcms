fc11896d9cccb8e62fc2280d8127e40018aa47c5
thomas_kneip <thomas_kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-10-13 14:24:54 +0000

    WE-410

--- webEdition/we/include/copyFolder.inc.php
+++ webEdition/we/include/copyFolder.inc.php
@@ -65,7 +65,7 @@ class copyFolderFrag extends taskFragment
 			
 			// make it twice to be sure that all linked IDs are correct
 			$db->query(
-					"SELECT ID,ParentID,Text,Path,IsFolder,ClassName,ContentType,Category FROM " . FILE_TABLE . " WHERE (Path like'$fromPath/%') AND ContentType != 'text/webedition' ORDER BY IsFolder DESC,Path");
+					"SELECT ID,ParentID,Text,Path,IsFolder,ClassName,ContentType,Category FROM " . FILE_TABLE . " WHERE (Path like'".mysql_real_escape_string($fromPath)."/%') AND ContentType != 'text/webedition' ORDER BY IsFolder DESC,Path");
 			while ($db->next_record()) {
 				$db->Record["CopyToId"] = $toID;
 				$db->Record["CopyFromId"] = $fromID;
@@ -81,7 +81,7 @@ class copyFolderFrag extends taskFragment
 			
 			for ($num = 0; $num < 2; $num++) {
 				$db->query(
-						"SELECT ID,ParentID,Text,TemplateID,Path,IsFolder,ClassName,ContentType,Category FROM " . FILE_TABLE . " WHERE (Path like'$fromPath/%') AND ContentType = 'text/webedition' ORDER BY IsFolder DESC,Path");
+						"SELECT ID,ParentID,Text,TemplateID,Path,IsFolder,ClassName,ContentType,Category FROM " . FILE_TABLE . " WHERE (Path like'".mysql_real_escape_string($fromPath)."/%') AND ContentType = 'text/webedition' ORDER BY IsFolder DESC,Path");
 				while ($db->next_record()) {
 					
 					// check if the template exists
@@ -150,7 +150,7 @@ class copyFolderFrag extends taskFragment
 			$we_doc->initByID($this->data["ID"]);
 			// if file  exists the file will overwritten, if not a new one (with no id) will be created
 			$we_doc->ID = f(
-					"SELECT ID FROM " . FILE_TABLE . " WHERE Path='" . $path . "'", 
+					"SELECT ID FROM " . FILE_TABLE . " WHERE Path='" . mysql_real_escape_string($path) . "'", 
 					"ID", 
 					$GLOBALS["DB_WE"]);
 			$we_doc->Path = $path;
@@ -206,13 +206,13 @@ class copyFolderFrag extends taskFragment
 							$dt->DocType = $dt->DocType . "_copy";
 							// if file exists we need  to create a new one!
 							if ($file_id = f(
-									"SELECT ID FROM " . DOC_TYPES_TABLE . " WHERE DocType='" . $dt->DocType . "'", 
+									"SELECT ID FROM " . DOC_TYPES_TABLE . " WHERE DocType='" . mysql_real_escape_string($dt->DocType) . "'", 
 									"ID", 
 									$GLOBALS["DB_WE"])) {
 								$z = 0;
 								$footext = $dt->DocType . "_" . $z;
 								while (f(
-										"SELECT ID FROM " . DOC_TYPES_TABLE . " WHERE DocType='$footext'", 
+										"SELECT ID FROM " . DOC_TYPES_TABLE . " WHERE DocType='".mysql_real_escape_string($footext)."'", 
 										"ID", 
 										$GLOBALS["DB_WE"])) {
 									$z++;
@@ -297,13 +297,13 @@ class copyFolderFrag extends taskFragment
 			$templ->Path = $templ->getParentPath() . (($templ->getParentPath() != "/") ? "/" : "") . $templ->Text;
 			// if file exists we need  to create a new one!
 			if ($file_id = f(
-					"SELECT ID FROM " . TEMPLATES_TABLE . " WHERE Path='" . $templ->Path . "'", 
+					"SELECT ID FROM " . TEMPLATES_TABLE . " WHERE Path='" . mysql_real_escape_string($templ->Path) . "'", 
 					"ID", 
 					$GLOBALS["DB_WE"])) {
 				$z = 0;
 				$footext = $templ->Filename . "_" . $z . $templ->Extension;
 				while (f(
-						"SELECT ID FROM " . TEMPLATES_TABLE . " WHERE Text='$footext' AND ParentID='" . $templ->ParentID . "'", 
+						"SELECT ID FROM " . TEMPLATES_TABLE . " WHERE Text='".mysql_real_escape_string($footext)."' AND ParentID='" . abs($templ->ParentID) . "'", 
 						"ID", 
 						$GLOBALS["DB_WE"])) {
 					$z++;
@@ -616,7 +616,7 @@ class copyFolderFrag extends taskFragment
 		if ($int) {
 			$nintID = $n . "_we_jkhdsf_intID";
 			eval('$intID = ' . $fn . '($nintID);');
-			return f("SELECT Path FROM " . FILE_TABLE . " WHERE ID='$intID'", "Path", $db);
+			return f("SELECT Path FROM " . FILE_TABLE . " WHERE ID='".abs($intID)."'", "Path", $db);
 		} else {
 			eval('$extPath = ' . $fn . '($n);');
 			return $extPath;
@@ -671,7 +671,7 @@ class copyFolderFrag extends taskFragment
 
 	function getID($path, $db)
 	{
-		return f("SELECT ID FROM " . FILE_TABLE . " WHERE Path='$path'", "ID", $db);
+		return f("SELECT ID FROM " . FILE_TABLE . " WHERE Path='".mysql_real_escape_string($path)."'", "ID", $db);
 	}
 
 	function getPid($path, $db)
@@ -680,7 +680,7 @@ class copyFolderFrag extends taskFragment
 		if ($path == "/") {
 			return 0;
 		}
-		return f("SELECT ID FROM " . FILE_TABLE . " WHERE Path='$path'", "ID", $db);
+		return f("SELECT ID FROM " . FILE_TABLE . " WHERE Path='".mysql_real_escape_string($path)."'", "ID", $db);
 	}
 
 	function getDocument()
