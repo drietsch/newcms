f4c66e1405b17b2bb13ec730fc0fe74fe50a73d2
denic <denic@5781a423-4729-0410-aacd-f88070b95415>
2008-10-22 07:53:32 +0000

    #WE-515

--- webEdition/webEdition.php
+++ webEdition/webEdition.php
@@ -1264,7 +1264,7 @@ function we_cmd() {
 			return 0;
 			break;
 		case "setEconda":
-			new jsWindow(cmsurl+"econdasettings","setEconda",-1,-1,500,400,true,false,true);
+			new jsWindow(cmsurl+"Econdasettings","setEconda",-1,-1,500,400,true,false,true);
 			break;
 		<?php
 		pWebEdition_JSwe_cmds();
