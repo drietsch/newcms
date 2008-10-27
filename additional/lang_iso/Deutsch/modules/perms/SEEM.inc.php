19d4d62ad02d3307b48181e1f7178c51437714b9
tequillaymota <tequillaymota@5781a423-4729-0410-aacd-f88070b95415>
2008-10-27 09:22:55 +0000

    Version 5.9.0.3

---
+++ additional/lang_iso/Deutsch/modules/perms/SEEM.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+// +----------------------------------------------------------------------+
+// | webEdition                                                           |
+// +----------------------------------------------------------------------+
+// | PHP version 4.1.0 or greater                                         |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2000 - 2007 living-e AG                                |
+// +----------------------------------------------------------------------+
+//
+
+
+$l_perm["seem"]["perm_group_title"] = "seeMode";
+
+$l_perm["seem"]["CAN_SEE_MENUE"] = "Der Benutzer darf die Menü-Leiste sehen.";
+$l_perm["seem"]["CAN_WORK_NORMAL_MODE"] = "Der Benutzer darf ausserhalb des seeMode arbeiten.";
+$l_perm["seem"]["CHANGE_START_DOCUMENT"] = "Der Benutzer darf sein Startdokument ändern.";
+
+?>
---
+++ additional/lang_iso/Deutsch/modules/we_tabs.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+// +----------------------------------------------------------------------+
+// | webEdition                                                           |
+// +----------------------------------------------------------------------+
+// | PHP version 4.1.0 or higher                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2000 - 2005 living-e AG                   |
+// +----------------------------------------------------------------------+
+//
+
+
+/**************************************************************************
+ * For Module Workflow
+ **************************************************************************/
+$l_tabs["module"]["overview"]   = "Übersicht";
+$l_tabs["module"]["properties"] = "Eigenschaften";
+
+/**************************************************************************
+ * For Module User
+ **************************************************************************/
+$l_tabs["module"]["data"]        = "Daten";
+$l_tabs["module"]["permissions"] = "Rechte";
+$l_tabs["module"]["workspace"]   = "Arbeitsbereich";
+$l_tabs["module"]["preferences"] = "Einstellungen";
+
+/**************************************************************************
+ * For Banner User
+ **************************************************************************/
+$l_tabs["module"]["placement"]  = "Platzierung";
+$l_tabs["module"]["statistics"] = "Statistik";
+
+/**************************************************************************
+ * For Shop Module
+ **************************************************************************/
+$l_tabs["module"]["orderlist"]  = "Alle Bestellungen des Kunden";
+$l_tabs["module"]["orderdata"]  = "Bestell- und Kundendaten";
+$l_tabs["module"]["revenueTotal"]  = "Umsatz im Jahr %s";
+$l_tabs["module"]["artList"]  = "Auflistung aller Artikel: %s"; 
+
+$l_tabs["module"]["PayMethodEdit"]  = "Zahlungsarten bearbeiten";
+$l_tabs["module"]["ArtAttribEdit"]  = "Artikel-Eigenschaften bearbeiten";
+
+$l_tabs["module"]["admin_1"]  = "Artikel-Dokumente";
+$l_tabs["module"]["admin_2"]  = "Artikel-Objekte";
+$l_tabs["module"]["admin_3"]  = "Umsatzübersicht";
+
+
+$l_tabs["module"]["admin_6"]  = "Zahlungsarten";
+?>
