a1caa42f946f40731043d8eb2a8bf27fcad523d3
holger_meyer <holger_meyer@5781a423-4729-0410-aacd-f88070b95415>
2008-10-27 11:01:54 +0000

    WE-517

--- webEdition/we/include/we_language/French_UTF-8/backup.inc.php
+++ webEdition/we/include/we_language/French_UTF-8/backup.inc.php
@@ -34,6 +34,7 @@ $l_backup["settings"] = "Restaurer les préférences";
 $l_backup["rebuild"] = "Rebuild automatique";
 $l_backup["select_upload_file"] = "Restauration d'un fichier local";
 $l_backup["select_server_file"] = "Choisissez le fichier de sauvegarde que vous voulez restaurer.";
+$l_backup["charset_warning"] = "If you encounter problems when you restore a backup, please ensure that the target system has setup the same character set as the source system. This applies both to the character set of the database (collation), as well as for the character set of the user interface language!"; // TRANSLATE
 $l_backup["finished_success"] = "La restauration de la sauvegarde a été terminé avec succès.";
 $l_backup["finished_fail"] = "La restauration de la sauvegarde n'a pas été terminé avec succès.";
 $l_backup["question_taketime"] = "L\\'export demande quelques temps.";
