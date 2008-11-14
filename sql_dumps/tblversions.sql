63c3559c0faabe4046d77aa0653115864ee1d03a
thomas_kneip <thomas_kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-11-14 15:44:58 +0000

    WE-573

--- sql_dumps/tblversions.sql
+++ sql_dumps/tblversions.sql
@@ -47,5 +47,6 @@ CREATE TABLE `tblversions` (
   `fromScheduler` tinyint(1) NOT NULL,
   `fromImport` tinyint(1) NOT NULL,
   `resetFromVersion` bigint(20) NOT NULL,
+  `InGlossar` tinyint(1) NOT NULL,
   PRIMARY KEY  (`ID`)
 ) TYPE=MyISAM ;
