3bb43d4fec894916eca0588a20e90f66309af3ae
thomas_kneip <thomas_kneip@5781a423-4729-0410-aacd-f88070b95415>
2008-12-04 14:01:26 +0000

    check java time

--- webEdition/headermenu.php
+++ webEdition/headermenu.php
@@ -116,7 +116,7 @@ if ( !isset($_REQUEST["SEEM_edit_include"]) ) { // there is only a menu when not
 
 			function checkAndLoadAltMenu() {
 				if (appletTrys < 10) {
-					setTimeout("checkApplet()",300);
+					setTimeout("checkApplet()",1000);
 				} else {
 					document.location = "headermenu.php?showAltMenu=true";
 				}
