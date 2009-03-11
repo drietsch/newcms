a2b0bc9928f1010e97303637601fab056d5fa77c
thomas_kneip <thomas_kneip@5781a423-4729-0410-aacd-f88070b95415>
2009-03-11 18:02:15 +0000

    add comment

--- webEdition/apps/toolfactory/controllers/RpcController.php
+++ webEdition/apps/toolfactory/controllers/RpcController.php
@@ -27,6 +27,7 @@ class RpcController extends Zend_Controller_Action
 	public function indexAction()
 	{
 		$jsonOutput = we_net_rpc_JsonRpc::getReply('toolfactory');
+		//response for callback
 		$this->getResponse()->setHeader('Content-Type', 'application/json')->appendBody($jsonOutput);
 	}
 
